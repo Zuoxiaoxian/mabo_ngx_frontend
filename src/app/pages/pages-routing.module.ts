8dd5d17ef475aa235f99dc5027fcf1957a210aad
ziserendexin <32018376+ziserendexin@users.noreply.github.com>
2020-11-17 12:21:24 +0800

    去掉无关页面

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -4,15 +4,14 @@
  * @Author: Zhang Hengye
  * @Date: 2020-11-04 10:55:43
  * @LastEditors: Zhang Hengye
- * @LastEditTime: 2020-11-06 14:37:17
+ * @LastEditTime: 2020-11-17 12:20:54
  */
 import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { PagesComponent } from './pages.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ECommerceComponent } from './e-commerce/e-commerce.component';
 import { Test_moduleComponent } from './test_module/test_module.component';
+// import { VideoBasePageComponent } from './video-base-page/video-base-page.component';
 import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
 
 const routes: Routes = [{
@@ -20,72 +19,20 @@ const routes: Routes = [{
   component: PagesComponent,
   children: [
     {
-      path: 'dashboard',
-      component: ECommerceComponent,
-    },
-    {
-      path: 'iot-dashboard',
-      component: DashboardComponent,
-    },
-    {
       path: 'test_module',
       // loadChildren: () => import('./test_module/test_module.module')
       //   .then(m => m.Test_moduleModule),
       component: Test_moduleComponent,
     },
-    {
-      path: 'layout',
-      loadChildren: () => import('./layout/layout.module')
-        .then(m => m.LayoutModule),
-    },
-    {
-      path: 'forms',
-      loadChildren: () => import('./forms/forms.module')
-        .then(m => m.FormsModule),
-    },
-    {
-      path: 'ui-features',
-      loadChildren: () => import('./ui-features/ui-features.module')
-        .then(m => m.UiFeaturesModule),
-    },
-    {
-      path: 'modal-overlays',
-      loadChildren: () => import('./modal-overlays/modal-overlays.module')
-        .then(m => m.ModalOverlaysModule),
-    },
-    {
-      path: 'extra-components',
-      loadChildren: () => import('./extra-components/extra-components.module')
-        .then(m => m.ExtraComponentsModule),
-    },
-    {
-      path: 'maps',
-      loadChildren: () => import('./maps/maps.module')
-        .then(m => m.MapsModule),
-    },
-    {
-      path: 'charts',
-      loadChildren: () => import('./charts/charts.module')
-        .then(m => m.ChartsModule),
-    },
-    {
-      path: 'editors',
-      loadChildren: () => import('./editors/editors.module')
-        .then(m => m.EditorsModule),
-    },
-    {
-      path: 'tables',
-      loadChildren: () => import('./tables/tables.module')
-        .then(m => m.TablesModule),
-    },
-    {
-      path: 'miscellaneous',
-      loadChildren: () => import('./miscellaneous/miscellaneous.module')
-        .then(m => m.MiscellaneousModule),
-    },
+    // {
+    //   path: 'video-base-page',
+    //   // loadChildren: () => import('./test_module/test_module.module')
+    //   //   .then(m => m.Test_moduleModule),
+    //   component: VideoBasePageComponent,
+    // },
     {
       path: '',
-      redirectTo: 'dashboard',
+      redirectTo: 'test_module',
       pathMatch: 'full',
     },
     {
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,18 @@
+
+
+/*
+ * @Author: Zhang Hengye
+ * @Date: 2020-11-04 10:55:43
+ * @LastEditors: Zhang Hengye
+ * @LastEditTime: 2020-11-17 11:22:40
+ */
 import { NgModule } from '@angular/core';
 import { NbMenuModule } from '@nebular/theme';
 
 import { ThemeModule } from '../@theme/theme.module';
 import { PagesComponent } from './pages.component';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { ECommerceModule } from './e-commerce/e-commerce.module';
+// import { DashboardModule } from './dashboard/dashboard.module';
+// import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
 
@@ -13,8 +21,8 @@ import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
     PagesRoutingModule,
     ThemeModule,
     NbMenuModule,
-    DashboardModule,
-    ECommerceModule,
+    // DashboardModule,
+    // ECommerceModule,
     MiscellaneousModule,
   ],
   declarations: [
